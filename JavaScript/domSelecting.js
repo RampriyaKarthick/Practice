deee1e81976ccf481d5a04efd2363cb9b6fdd5ba
RampriyaKarthick <118220230+RampriyaKarthick@users.noreply.github.com>
2025-12-15 12:57:57 +0100

    dom select

---
+++ JavaScript/domSelecting.js
@@ -0,0 +1,16 @@
+// console.log(document.head)
+
+let heading = document.getElementsByTagName("h1")
+console.log(heading)
+
+let para = document.getElementsByClassName("para1")
+console.log(para)
+
+let uniqueId= document.getElementById("uniqueId")
+console.log(uniqueId)
+
+let inputTag = document.getElementsByName("input-1")
+console.log(inputTag)
+
+let querySel = document.querySelector(".para1")
+console.log(querySel)
