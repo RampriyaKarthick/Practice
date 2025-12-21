976e3538048761200ab6c6c22416ec140f8b55a4
RampriyaKarthick <118220230+RampriyaKarthick@users.noreply.github.com>
2025-12-21 13:30:04 +0100

    form-validation-frontend

---
+++ form-validation/form.js
@@ -0,0 +1,4 @@
+let userName = document.getElementById("username");
+let password = document.getElementById("password");
+let confirmPassword = document.getElementById("confirm-password");
+let email = document.getElementById("email");
